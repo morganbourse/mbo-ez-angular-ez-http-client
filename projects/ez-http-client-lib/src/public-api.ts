5dd8b096a7e62af2168654a9174137a25e8b4ecb
Morgan B <morgan.bourse@gmail.com>
2024-08-29 22:09:40 +0200

    :bug: Export publicly EzHttpHeader decorator

--- projects/ez-http-client-lib/src/public-api.ts
+++ projects/ez-http-client-lib/src/public-api.ts
@@ -9,6 +9,7 @@ export * from './lib/models/ez-http-client-response-oeprators-options.model';
 export * from './lib/ez-http-client.decorator';
 export * from './lib/ez-http-client-headers.decorator';
 export * from './lib/ez-http-client-common-response-operators.decorator';
+export * from './lib/ez-http-header.decorator';
 export * from './lib/ez-http-query-param.decorator';
 export * from './lib/ez-http-part-data.decorator';
 export * from './lib/ez-http-part-file.decorator';
